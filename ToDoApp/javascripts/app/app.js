30842c2e1ef387740acbc42ceab283482f585541
Jack Fortenbery <jaforten@unca.edu>
2013-03-20 18:47:08 -0400

    added category tab in seperate div and attatched click function that gets the json data.

--- ToDoApp/javascripts/app/app.js
+++ ToDoApp/javascripts/app/app.js
@@ -8,21 +8,14 @@ var main = function () {
       return false;
     });    
   };
-  
-  var setUpClickHandlerCategory = function (category) {
-    category.click(function () {
-      var target = $(this).attr("href");
-      $(".active").removeClass("active");
-      $(this).addClass("active");
-      $("#"+target).addClass("active");
-      return false;
-    });    
-  };
+
+  //content remove  
+
 //tab:
   var tabclick= $(".tabs");
+ 
   setUpClickHandler(tabclick);
   
-
 //content input:
   $(".submit").click(function () {
     
@@ -30,36 +23,38 @@ var main = function () {
     var categories = $("#categories").val().replace(",","");
     
     $("<li id= input>" + "<button id= remove>" + "x" + "</button>" + "<font size= 6, color= blue>" + description + "</font>" + " " +
-      "<font size= 4, color= red>" + categories + "</font>" + "</li>").prependTo("ul");
+      "<font size= 4, color= red>" + categories + "</font>" + "</li>").prependTo(" #all ul");
     
     
     alert("Added To List!");
     
   });
-//content display:
+//content display: all tab
 
   $.getJSON("tabs/all.json",  function(data) {
         alert("success");
         
         data.forEach(function (todo) {
-          var label = [{}] 
+           
           var category = []        
           description = todo.description
          
           category = todo.categories.join(" ")
           
           $( "<li id= 'descript'>" + "<button id= 'remove'>" + "x" + "</button>" + "<font size= 6, color= blue>" + description + "</font>"  + 
-             "  " + "<font size= 4, color= red>" + category + "</font>"  + "</li>").appendTo("ul");
-          
-          
-           
-         
+             "  " + "<font size= 4, color= red>" + category + "</font>"  + "</li>").appendTo("#all ul");
         });
-         
   });
   
-  //content remove
-  
+ //content display: categorize tab
+ var setUpClickHandlerCat = function (cat) {
+    cat.click(function () {
+      $.getJSON("tabs/all.json", function(data){
+        alert("success");
+      });
+    });
+ };
+//remove   
   $(document).on('click', "#remove",function () {
     $(this).parent("li").remove();
     
@@ -67,6 +62,7 @@ var main = function () {
   });
 
   setUpClickHandler($(".tabs .tab"));
+  setUpClickHandlerCat($(".category .tab"));
 };
 
 $(document).ready(main);
